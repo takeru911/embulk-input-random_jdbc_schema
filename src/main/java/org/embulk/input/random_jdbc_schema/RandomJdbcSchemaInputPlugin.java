214f57eb30637432c33db66cd145e90740885f9e
takeru911 <takeru_maehara@voyagegroup.com>
2017-04-23 12:37:06 +0900

    remove columns option

--- src/main/java/org/embulk/input/random_jdbc_schema/RandomJdbcSchemaInputPlugin.java
+++ src/main/java/org/embulk/input/random_jdbc_schema/RandomJdbcSchemaInputPlugin.java
@@ -1,7 +1,6 @@
 package org.embulk.input.random_jdbc_schema;
 
 import java.util.List;
-import java.util.Locale;
 
 import org.embulk.config.Config;
 import org.embulk.config.ConfigDefault;
@@ -10,95 +9,113 @@ import org.embulk.config.ConfigSource;
 import org.embulk.config.Task;
 import org.embulk.config.TaskReport;
 import org.embulk.config.TaskSource;
+import org.embulk.plugin.PluginType;
 import org.embulk.spi.Column;
 import org.embulk.spi.Exec;
+import org.embulk.spi.ExecSession;
 import org.embulk.spi.InputPlugin;
 import org.embulk.spi.PageBuilder;
 import org.embulk.spi.PageOutput;
 import org.embulk.spi.Schema;
 import org.embulk.spi.SchemaConfig;
 import org.embulk.spi.time.Timestamp;
-import org.embulk.spi.time.TimestampFormatter;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.jruby.util.RubyDateFormat;
+import org.slf4j.Logger;
 
 public class RandomJdbcSchemaInputPlugin
-        implements InputPlugin
+implements InputPlugin
 {
-    public interface PluginTask
-            extends Task
-    {
-        @Config("columns")
-        public SchemaConfig getColumns();
-        
-        @Config("numOfRow")
-        @ConfigDefault("1000")
-        public int getNumOfRow();
-    }
+	private final Logger logger = Exec.getLogger(RandomJdbcSchemaInputPlugin.class);
+	public interface PluginTask
+	extends Task
+	{
+		@Config("numOfRow")
+		@ConfigDefault("1000")
+		public int getNumOfRow();
+	}
 
-    @Override
-    public ConfigDiff transaction(ConfigSource config,
-            InputPlugin.Control control)
-    {
-        PluginTask task = config.loadConfig(PluginTask.class);
-        
-        Schema schema = task.getColumns().toSchema();
-        int taskCount = 1;  // number of run() method calls
-        
-        return resume(task.dump(), schema, taskCount, control);
-    }
+	@Override
+	public ConfigDiff transaction(ConfigSource config,
+			InputPlugin.Control control)
+	{
+		PluginTask task = config.loadConfig(PluginTask.class);
+		Schema schemaFromJdbc;
+		try {
+			schemaFromJdbc = fetchSchemaByJDBC(config);
+			for (int i = 0; i < schemaFromJdbc.getColumnCount(); i++) {
+				Column column = schemaFromJdbc.getColumn(i);
+				logger.info(String.format("column %d : name = %s, type = %s", i + 1, column.getName(), column.getType()));
+			}
+			config.set("columns", schemaFromJdbc.getColumns());
+			int taskCount = 1;  // number of run() method calls
 
-    @Override
-    public ConfigDiff resume(TaskSource taskSource,
-            Schema schema, int taskCount,
-            InputPlugin.Control control)
-    {
-        control.run(taskSource, schema, taskCount);
-        return Exec.newConfigDiff();
-    }
+			return resume(task.dump(), schemaFromJdbc, taskCount, control);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-    @Override
-    public void cleanup(TaskSource taskSource,
-            Schema schema, int taskCount,
-            List<TaskReport> successTaskReports)
-    {
-    }
+	private Schema fetchSchemaByJDBC(ConfigSource config) throws Exception{
+		ExecSession session = Exec.session();
+		ConfigSource child = config.getNested("schema");
+		String type = child.get(String.class, "type");
+		InputPlugin input = session.newPlugin(InputPlugin.class, new PluginType(type));
+		JdbcInputPluginHelper helper;
+		helper = new JdbcInputPluginHelper(input);
+		final Schema schemaFromJdbc = helper.getSchema(child);
+		return schemaFromJdbc;
+	}
 
-    @Override
-    public TaskReport run(TaskSource taskSource,
-            Schema schema, int taskIndex,
-            PageOutput output)
-    {
-        PluginTask task = taskSource.loadTask(PluginTask.class);
-        int numOfRow = task.getNumOfRow();
-        final PageBuilder pageBuilder = new PageBuilder(Exec.getBufferAllocator(), schema, output);
+	@Override
+	public ConfigDiff resume(TaskSource taskSource,
+			Schema schema, int taskCount,
+			InputPlugin.Control control)
+	{
+		control.run(taskSource, schema, taskCount);
+		return Exec.newConfigDiff();
+	}
 
-        for(int i = 0; i < numOfRow; i++){
-            for(int j = 0; j < schema.getColumnCount(); j++){
-            	Column column = schema.getColumn(j);
-            	if(column.getType().getName().equals("long")){
-            		pageBuilder.setLong(schema.getColumn(j), i);
-            	}else if(column.getType().getName().equals("double")){
-            		pageBuilder.setDouble(schema.getColumn(j), i);
-                }else if(column.getType().getName().equals("string")){
-            		pageBuilder.setString(schema.getColumn(j), "hoge");
-                }else if(column.getType().getName().equals("timestamp")){
-                	pageBuilder.setTimestamp(schema.getColumn(j), Timestamp.ofEpochSecond(System.currentTimeMillis() / 1000));
-                }else if(column.getType().getName().equals("boolean")){
-            		pageBuilder.setBoolean(schema.getColumn(j), false);
-                }
-            }
-            pageBuilder.addRecord();            
-        }	
-        pageBuilder.finish();
-        pageBuilder.close();
-        return Exec.newTaskReport();
-    }
+	@Override
+	public void cleanup(TaskSource taskSource,
+			Schema schema, int taskCount,
+			List<TaskReport> successTaskReports)
+	{
+	}
 
-    @Override
-    public ConfigDiff guess(ConfigSource config)
-    {
-        return Exec.newConfigDiff();
-    }
+	@Override
+	public TaskReport run(TaskSource taskSource,
+			Schema schema, int taskIndex,
+			PageOutput output)
+	{
+		PluginTask task = taskSource.loadTask(PluginTask.class);
+		int numOfRow = task.getNumOfRow();
+		final PageBuilder pageBuilder = new PageBuilder(Exec.getBufferAllocator(), schema, output);
+
+		for(int i = 0; i < numOfRow; i++){
+			for(int j = 0; j < schema.getColumnCount(); j++){
+				Column column = schema.getColumn(j);
+				if(column.getType().getName().equals("long")){
+					pageBuilder.setLong(schema.getColumn(j), i);
+				}else if(column.getType().getName().equals("double")){
+					pageBuilder.setDouble(schema.getColumn(j), i);
+				}else if(column.getType().getName().equals("string")){
+					pageBuilder.setString(schema.getColumn(j), "hoge");
+				}else if(column.getType().getName().equals("timestamp")){
+					pageBuilder.setTimestamp(schema.getColumn(j), Timestamp.ofEpochSecond(System.currentTimeMillis() / 1000));
+				}else if(column.getType().getName().equals("boolean")){
+					pageBuilder.setBoolean(schema.getColumn(j), false);
+				}
+			}
+			pageBuilder.addRecord();            
+		}	
+		pageBuilder.finish();
+		pageBuilder.close();
+		return Exec.newTaskReport();
+	}
+
+	@Override
+	public ConfigDiff guess(ConfigSource config)
+	{
+		return Exec.newConfigDiff();
+	}
 }
